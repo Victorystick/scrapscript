a2cda637c183c779bfed3851888c846b087c6395
Oskar Segersvärd <victorystick@gmail.com>
2025-05-24 15:55:19 +0200

    Add colors to printed errors.

--- token/error.go
+++ token/error.go
@@ -12,10 +12,31 @@ type Error struct {
 	Msg   string
 }
 
+var errorFormat = fmt.Sprintf(
+	"%s: %%s\n\n%s: %%s\n%%s%s",
+	color(red, "error"),
+	color(yellow, "%5d"),
+	color(red, "%s"))
+
 func (e Error) Error() string {
 	column := e.Pos.Column - 1
 	lineLength := min(len(e.Line)-column, e.Range.Len())
 	return fmt.Sprintf(
-		"%s\n%6d: %s\n%s%s",
-		e.Msg, e.Pos.Line, e.Line, strings.Repeat(" ", 8+column), strings.Repeat("^", lineLength))
+		errorFormat, e.Msg, e.Pos.Line, e.Line, strings.Repeat(" ", 7+column), strings.Repeat("~", lineLength))
+}
+
+type Color rune
+
+const (
+	red = 31 + iota
+	green
+	yellow
+	blue
+	purple
+	teal
+	gray
+)
+
+func color(color Color, text string) string {
+	return fmt.Sprintf("\033[%dm%s\033[m", color, text)
 }
